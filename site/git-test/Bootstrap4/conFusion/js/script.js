91fd9503f9d0964cab21cd179ee522678300ec7f
priyal-shah <priyalajayshah@gmail.com>
2021-07-22 14:53:00 +0530

    update

---
+++ site/git-test/Bootstrap4/conFusion/js/script.js
@@ -0,0 +1,9 @@
+/*reserve modal JS*/
+        $('#reservetablebtn').on('click', function (){
+            $('#reservetablemodal').modal('show');
+        });
+
+        /*login modal JS*/
+        $('#loginbtn').on('click', function (){
+            $('#loginmodal').modal('show');
+        });
