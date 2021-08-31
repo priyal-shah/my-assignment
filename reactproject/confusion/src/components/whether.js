b630e8004343e3c618e66b0bb806e8c461c2a27b
priyal-shah <priyalajayshah@gmail.com>
2021-08-31 11:35:22 +0530

    update

--- reactproject/confusion/src/components/whether.js
+++ reactproject/confusion/src/components/whether.js
@@ -1,35 +1,58 @@
-import React from 'react';
+//api.openweathermap.org/data/2.5/weather?q=pune&appid=c670203272ce32894d4aba4a643058b4
+
+import React, {useState, useEffect} from 'react';
+import WeatherCard from './WeatherCard';
  
 const Weather=()=>{
+    const [searchvalue, setsearchvalue]=useState("pune");
+    const [tempinfo, settempinfo]=useState({});
+
+
+    const getweatherinfo=async()=>{
+
+        try{
+            let url=`https://api.openweathermap.org/data/2.5/weather?q=${searchvalue}&units=metric&appid=c670203272ce32894d4aba4a643058b4`
+
+            const res=await fetch(url);
+            const data=await res.json();
+
+            const{temp,pressure, humidity}=data.main;
+            const{speed}=data.wind;
+            const{country, sunset}=data.sys;
+            const{name}=data;
+            const{main:weathermood}=data.weather[0];
+
+            const newweatherinfo={
+                temp,pressure, humidity,speed, country, sunset, name, weathermood
+            };
+
+            settempinfo(newweatherinfo);
+
+
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+
+    useEffect( getweatherinfo );
+
+
     return(
         <>
            <div className="wrap">
                <div className="search">
-                   <input type="search" className="searchTerm" placeholder="✍search...." autofocus id="search"></input>
-                   <button className="searchButton">search</button>
+                   <input type="search" className="searchTerm" placeholder="✍search...." autoFocus id="search"  
+                     value={searchvalue}
+                     onChange={(e)=>setsearchvalue(e.target.value)}
+                   ></input>
+                   <button className="searchButton" onClick={getweatherinfo}>search</button>
                </div>
            </div>
 
            <div>
-                <article className="widget">
-                    <div className="weatherIcon">
-                       <i className={"wi wi-day-sunny"}></i>
-                    </div>
-
-                    <div className="weatherInfo">
-                         <div className="temperature">
-                         <span>25.5&deg;</span>
-                         </div>
-
-                         <div className="description">
-                             <div className="weatherCondition">
-                             sunny
-                             </div>
-                             <div className="place"> Pune, India</div>
-                         </div>
-                    </div>
-                    <div className="date">{new Date().toLocaleString()}</div>
-                </article>
+                <WeatherCard   tempinfo={tempinfo} />
            </div>
         </>
     	);
